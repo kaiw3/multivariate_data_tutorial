81fe60d6460c8d064b1672054d97be42a7f78a90
kaiw3 <s1727718@ed.ac.uk>
2020-12-04 02:07:21 +0000

    KW Structure changes

--- script/tutorial_code.R
+++ script/tutorial_code.R
@@ -5,7 +5,6 @@
 
 # Load packages
 library(vegan)  # For performing the nmds
-library(gplots)  # For heatmaps
 library(tidyverse)  # For data tidying functions and plotting
 
 # Load data
@@ -75,71 +74,19 @@ ggsave(filename = "outputs/nmds_3.png", nmds.plot.barents.3, height = 7, width =
 # Species info shows some species are more commonly found in different conditions.
 # None of this is statistically verified yet, so a ANOSIM, SIMPER or indicator species analysis will need to be run to check.
 
+# ANOSIM
+mat_bar_spp <- as.matrix(barents_spp)
 
+# ANOSIM with depth grouping
+bar_depth <- anosim(mat_bar_spp, barents_env_raw$Depth, distance = "bray", permutations = 9999)
+bar_depth  # ANOSIM significance is less than 0.05 so it is significant. R statistic is relatively low, suggesting the groups are similar to each other.
 
+# ANOSIM with temperature grouping
+bar_temp <- anosim(mat_bar_spp, barents_env_raw$Temperature, distance = "bray", permutations = 9999)
+bar_temp  # Significance means this looks good too, and relatively low R statistic suggests similarity between groups.
 
-
-
-
-
-barents_pca$x
-barents_pca <- prcomp(barents_env_raw)
-summary(barents_pca)
-screeplot(barents_pca, type = "lines")
-(barents_pca$sdev)^2
-
-y <- matrix(rnorm(89)) 
-c <- cor(barents_spp, method="spearman")
-d <- as.dist(1-c)
-pear.dist <- as.dist((1-c)/2)
-levelplot(c)
-hp <- hclust(pear.dist, method="ward.D")
-plot(hp, hang=-1, cex=0.2)
-
-data.med.center <- sweep(barents_spp, MARGIN=1, STATS=apply(barents_spp,1,median), FUN = "-",) 
-data.med.center[ data.med.center > 5 ] <- 5
-data.med.center[ data.med.center < -5 ] <- -5
-
-pear.cols <- cor(t(data.med.center), method="pearson")
-pear.dist <- as.dist((1-pear.cols)/2)
-hc.cols <- hclust(pear.dist, method="ward.D")
-pear.rows <- cor(data.med.center, method="pearson")
-pear.dist <- as.dist((1-pear.rows)/2)
-hc.rows <- hclust(pear.dist, method="ward.D")
-
-heatmap.2(t(data.med.center), 
-          scale="none", 
-          cluster.by.col=TRUE, 
-          cluster.by.row=TRUE, 
-          Rowv=TRUE,
-          Colv=TRUE,
-          hclust.col = hc.cols, 
-          hclust.row = hc.rows, 
-          labCol=NULL,
-          cexRow = 0.5,
-          )
-
-
-# Failed NMDS
-community_matrix <- matrix(
-  sample(1:100,300,replace=T),nrow=10,
-  dimnames=list(paste("community",1:10,sep=""),paste("sp",1:30,sep="")))
-
-example_NMDS <- metaMDS(barents_spp, k = 2)
-stressplot(example_NMDS)        # Produce shepherds diagram to test goodness of fit
-
-plot(example_NMDS, "sites")   # Produces distance 
-orditorp(example_NMDS, "sites")
-colvec <- c("gray0", "gray0", "gray49", "gray49")   # Identifies colors for group assignments
-pchvec <- c(21, 21, 22, 22)
-
-ordiplot(example_NMDS,type="n")
-orditorp(example_NMDS,display="species",col="red",air=0.01)
-orditorp(example_NMDS,display="sites",cex=1.25,air=0.01)
-
-
-
-
-
-
+# Challenge data
+barents_env_chal <- barents_env_raw %>% 
+  mutate(lat_cat=cut(Latitude, breaks=c(-Inf, 72, 74, Inf), labels=c("south","centre","north"))) %>% 
+  mutate(long_cat=cut(Longitude, breaks=c(-Inf, 23, 30, Inf), labels=c("west","centre","east")))
 
